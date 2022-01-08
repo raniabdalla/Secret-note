6e1d6f68278e11c242356320ba73b2d0cdbd495b
Angela Dudley <angela.dudley14@gmail.com>
2022-01-07 18:54:29 -0600

    Gradle project started, Thymeleaf Dependency added.

---
+++ src/test/java/org/launchcode/secretnote/SecretNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.secretnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
