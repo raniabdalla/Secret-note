2af5cd35de3c82ebd0490ca9e7d261bc4d93107e
zacharybklein <zachbklein@gmail.com>
2022-01-10 20:50:32 -0600

    Added Folder, SecretNote, User models

---
+++ src/main/java/org/launchcode/secretnote/models/SecretNote.java
@@ -0,0 +1,44 @@
+package org.launchcode.secretnote.models;
+
+import java.util.UUID;
+
+public class SecretNote {
+
+    private String name;
+
+    private Folder folder;
+
+    private UUID id = UUID.randomUUID();
+
+    public SecretNote(String name, Folder folder) {
+        this.name = name;
+        this.folder = folder;
+        this.id = id;
+    }
+
+    public SecretNote() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Folder getFolder() {
+        return folder;
+    }
+
+    public void setFolder(Folder folder) {
+        this.folder = folder;
+    }
+
+    public UUID getId() { return id;}
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ src/main/java/org/launchcode/secretnote/models/User.java
@@ -0,0 +1,11 @@
+package org.launchcode.secretnote.models;
+
+import java.util.UUID;
+
+public class User {
+
+    private String username;
+
+    private UUID id = UUID.randomUUID();
+
+}
