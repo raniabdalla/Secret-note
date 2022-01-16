975e94f66574fa41e8207f70e1621a086b83b1c5
rania <raniaa13.rh@gmail.com>
2022-01-15 23:37:54 -0600

    add process registration form, and User entity(using UUID for Id).

--- src/main/java/org/launchcode/secretnote/models/User.java
+++ src/main/java/org/launchcode/secretnote/models/User.java
@@ -1,11 +1,43 @@
 package org.launchcode.secretnote.models;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
+@Entity
 public class User {
 
+    @Id
+    private UUID id = UUID.randomUUID();
+
+    public UUID getId() {
+        return id;
+    }
+
+    @NotNull
     private String username;
 
-    private UUID id = UUID.randomUUID();
+    @NotNull
+    private String hPassword;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.hPassword = encoder.encode(password);
+    }
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isMatching(String password){
+        return encoder.matches(password, hPassword);
+    }
 
 }
