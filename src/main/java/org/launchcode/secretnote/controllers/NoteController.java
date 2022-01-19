9f1e5850754aed9fc63bba9320bb93e14bda4c26
zacharybklein <zachbklein@gmail.com>
2022-01-18 18:46:30 -0600

    refactor SecretNoteController to HomeController

---
+++ src/main/java/org/launchcode/secretnote/controllers/NoteController.java
@@ -0,0 +1,10 @@
+package org.launchcode.secretnote.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NoteController {
+
+
+
+}
--- src/main/java/org/launchcode/secretnote/models/SecretNote.java
+++ src/main/java/org/launchcode/secretnote/models/SecretNote.java
@@ -1,20 +1,27 @@
 package org.launchcode.secretnote.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 public class SecretNote {
 
+    @NotBlank(message = "Please enter a note name")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
 
     private Folder folder;
 
-    private String description;
+    @NotBlank(message = "Please enter a note")
+    @Size(min = 1, max = 500, message = "Note must be between 1 and 500 characters")
+    private String content;
 
     private UUID id = UUID.randomUUID();
 
-    public SecretNote(String name, Folder folder) {
+    public SecretNote(String name, Folder folder, String content, UUID id) {
         this.name = name;
         this.folder = folder;
+        this.content = content;
         this.id = id;
     }
 
