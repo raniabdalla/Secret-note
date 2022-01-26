e3274e3ea1f03e4e5d81a3a42b8316fc32136125
swankidelic <swankidelic@gmail.com>
2022-01-25 18:52:12 -0600

    pulling caleb's updates from today

--- src/main/java/org/launchcode/secretnote/controllers/DashboardController.java
+++ src/main/java/org/launchcode/secretnote/controllers/DashboardController.java
@@ -1,2 +1,19 @@
-package org.launchcode.secretnote.controllers;public class DashboardController {
+package org.launchcode.secretnote.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DashboardController {
+    /**
+     * Main functionality page - dashboard to view and edit notes
+     *
+     */
+    @RequestMapping("dashboard")
+    public String index(Model model)
+    {
+        model.addAttribute("title", "Secret Note Dashboard");
+        return "dashboard";
+    }
 }
--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,48 +1,101 @@
-let notesRaw = '{
-                  "notes" : [
-                    {
-                      "id": 10001,
-                      "title": "Bobs Phone Number",
-                      "date_create": "2022-01-10",
-                      "date_edit": "2022-01-10",
-                      "content": "212-555-3030",
-                      "color": "#ffe599"
-                    },
-                    {
-                      "id": 10002,
-                      "title": "Walmart Shopping",
-                      "date_create": "2022-01-05",
-                      "date_edit": "2022-01-10",
-                      "content": "- shampoo<br>- painkillers<br>- parmesan cheese<br>- paper towels",
-                      "color": "#d5a6bd"
-                    },
-                    {
-                      "id": 10003,
-                      "title": "Alices D&D character",
-                      "date_create": "2022-01-03",
-                      "date_edit": "2022-01-07",
-                      "content": "Warlock Paladin<br>diety and patron the same - Corvea queen of ravens<br>|STR|12|<br>|DEX|9|<br>|CON|12|<br>|INT|10|<br>|WIS|13|<br>|CHA|16|",
-                      "color": "#f9cb9c"
-                    },
-                    {
-                      "id": 10004,
-                      "title": "Shopventory pwd",
-                      "date_create": "2022-01-06",
-                      "date_edit": "2022-01-08",
-                      "content": "store@<br>!nV3nt0ry<br><br>team@<br>1NV3ntory<br><br>company@<br>!nV3nt0ry1",
-                      "color": "#b4a7d6"
-                    },
-                    {
-                      "id": 10005,
-                      "title": "x",
-                      "date_create": "2022-01-02",
-                      "date_edit": "2022-01-02",
-                      "content": "wand of flaming sphere<br>three daggers<br>six vials of black powder<br>hat of disguise",
-                      "color": "#ea9999"
-                    }
-                  ]
-                }';
+window.addEventListener("load", function() {
 
-let newJSONobj = '{"name":"John","cars":{"mycar":"ford","borrowedcar":"audi"}}';
+  let note1 = {
+      id: 10001,
+      title: "Bobs Phone Number",
+      date_create: "2022-01-10",
+      date_edit: "2022-01-10",
+      content: "212-555-3030",
+      color: "#ffe599",
+      xsize: 200,
+      ysize: 180,
+      xpos: 50,
+      ypos: 50
+    }
 
-let notesObj = JSON.parse(notesRaw);
+  let note2 = {
+    id: 10002,
+    title: "Walmart Shopping",
+    date_create: "2022-01-05",
+    date_edit: "2022-01-10",
+    content: "- shampoo<br>- painkillers<br>- parmesan cheese<br>- paper towels",
+    color: "#d5a6bd",
+    xsize: 200,
+    ysize: 180,
+    xpos: 10,
+    ypos: 10
+  }
+
+// const notesObj = {note1, note2};
+  const notesRaw = `{
+                      {
+                        "id": 10001,
+                        "title": "Bobs Phone Number",
+                        "date_create": "2022-01-10",
+                        "date_edit": "2022-01-10",
+                        "content": "212-555-3030",
+                        "color": "#ffe599",
+                        "xsize": 200,
+                        "ysize": 180,
+                        "xpos": 20,
+                        "ypos": 20
+                      },
+                      {
+                        "id": 10002,
+                        "title": "Walmart Shopping",
+                        "date_create": "2022-01-05",
+                        "date_edit": "2022-01-10",
+                        "content": "- shampoo<br>- painkillers<br>- parmesan cheese<br>- paper towels",
+                        "color": "#d5a6bd",
+                        "xsize": 200,
+                        "ysize": 180,
+                        "xpos": 40,
+                        "ypos": 40
+                      },
+                      {
+                        "id": 10003,
+                        "title": "Alices D&D character",
+                        "date_create": "2022-01-03",
+                        "date_edit": "2022-01-07",
+                        "content": "Warlock Paladin<br>diety and patron the same - Corvea queen of ravens<br>|STR|12|<br>|DEX|9|<br>|CON|12|<br>|INT|10|<br>|WIS|13|<br>|CHA|16|",
+                        "color": "#f9cb9c",
+                        "xsize": 200,
+                        "ysize": 180,
+                        "xpos": 60,
+                        "ypos": 60
+                      },
+                      {
+                        "id": 10004,
+                        "title": "Shopventory pwd",
+                        "date_create": "2022-01-06",
+                        "date_edit": "2022-01-08",
+                        "content": "store@<br>!nV3nt0ry<br><br>team@<br>1NV3ntory<br><br>company@<br>!nV3nt0ry1",
+                        "color": "#b4a7d6",
+                        "xsize": 200,
+                        "ysize": 180,
+                        "xpos": 80,
+                        "ypos": 80
+                      },
+                      {
+                        "id": 10005,
+                        "title": "",
+                        "date_create": "2022-01-02",
+                        "date_edit": "2022-01-02",
+                        "content": "wand of flaming sphere<br>three daggers<br>six vials of black powder<br>hat of disguise",
+                        "color": "#ea9999",
+                        "xsize": 200,
+                        "ysize": 180,
+                        "xpos": 100,
+                        "ypos": 100
+                      }
+                    }`;
+    console.log(notesObj);
+
+    let stringifiedObj = JSON.stringify(notesObj);
+    console.log(stringifiedObj);
+
+    let parsedObj = JSON.parse(stringifiedObj);
+    console.log(parsedObj);
+
+
+});
