7c04d012342cee29e90c8532a0bac92ce9242baa
swankidelic <swankidelic@gmail.com>
2022-02-05 12:02:51 -0600

    view layouts mostly done, need zack's work on note details view

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -3,17 +3,26 @@ import notesRaw from "/js/testingNotes.js";
 function drawNote(note, zindex) {
     console.log(note.id);
 
-    let noteText = `<p class="noteTitle">${note.title}</p><p class="noteContent">${note.content}</p>`;
+//    let rowDiv = document.createElement("div");
+//    rowDiv.className = "row";
+//
+//    let noteAnchor = document.createElement("div")
+
+
+    let noteText = `<p class="noteTitle">${note.title}</p><hr><p class="noteContent">${note.content}</p>`;
 
     let noteDiv = document.createElement("div");
-    noteDiv.className = "note";
+    noteDiv.className = "col m-2 p-2";
     noteDiv.style.backgroundColor = note.color;
-    noteDiv.style.left = note.xpos;
-    noteDiv.style.top = note.ypos;
-    noteDiv.style.width = note.xsize;
+//    noteDiv.style.left = note.xpos;
+//    noteDiv.style.top = note.ypos;
+//    noteDiv.style.width = note.xsize;
     noteDiv.style.height = note.ysize;
-    noteDiv.style.zindex = zindex;
+//    noteDiv.style.zindex = zindex;
     noteDiv.innerHTML = noteText;
+
+
+
     dashboardContainer.appendChild(noteDiv);
 }
 
@@ -23,8 +32,12 @@ window.addEventListener("load", function() {
 
     const htmlBody = document.getElementsByTagName("body")[0];
     const dashboardContainer = document.getElementById("dashboardContainer");
+    dashboardContainer.className = "row p-3";
+//    dashboardContainer.appendChild()
+
 
     let zindex = 100;
+    let i = 0;
 
     for (let note of notesObj.notes) {
         drawNote(note, zindex);
