6fea23bfe78fc97f8343ed28ca758d821a1eb2ea
rania <raniaa13.rh@gmail.com>
2022-02-06 16:52:06 -0600

    create delete.html, add delete handlers

--- src/main/java/org/launchcode/secretnote/controllers/AccountsController.java
+++ src/main/java/org/launchcode/secretnote/controllers/AccountsController.java
@@ -1,19 +1,45 @@
 package org.launchcode.secretnote.controllers;
 
+import org.launchcode.secretnote.data.UserRepository;
 import org.launchcode.secretnote.models.LoginFormDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("account")
+@RequestMapping("accounts")
 public class AccountsController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("")
     public String displayEditForm(Model model){
         model.addAttribute(new LoginFormDTO());
         model.addAttribute("title", "Edit Account");
         return "accounts/index";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteAccountForm(Model model) {
+        model.addAttribute("title", "Delete Accounts");
+        model.addAttribute("users", userRepository.findAll());
+        return "accounts/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteAccountForm(@RequestParam(required = false) int[] userIds) {
+
+        if (userIds != null) {
+            for (int id : userIds) {
+                userRepository.deleteById(id);
+            }
+        }
+
+        return "redirect:/login";
     }
 }
