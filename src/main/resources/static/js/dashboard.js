73f3d04d59e170b925f2391dbe47c844e9fb4b21
swankidelic <swankidelic@gmail.com>
2022-02-07 00:48:12 -0600

    notes pull from database now

--- src/main/resources/static/js/dashboard.js
+++ src/main/resources/static/js/dashboard.js
@@ -1,19 +1,20 @@
-import notesRaw from "/js/testingNotes.js";
-
 function drawNote(note, zindex) {
-    console.log(note.id);
 
 //    let rowDiv = document.createElement("div");
 //    rowDiv.className = "row";
 //
 //    let noteAnchor = document.createElement("div")
 
+    var randomColor = function (obj) {
+        var keys = Object.keys(obj);
+        return obj[keys[ keys.length * Math.random() << 0]];
+    };
 
-    let noteText = `<p class="noteTitle">${note.title}</p><hr><p class="noteContent">${note.content}</p>`;
+    let noteText = `<div class="noteName">${note.name}</div><hr><p class="noteContent">${note.content}</p>`;
 
     let noteDiv = document.createElement("div");
-    noteDiv.className = "col m-2 p-2";
-    noteDiv.style.backgroundColor = note.color;
+    noteDiv.className = "col m-2 p-2 noteDrag";
+    noteDiv.style.backgroundColor = randomColor(Colors);
 //    noteDiv.style.left = note.xpos;
 //    noteDiv.style.top = note.ypos;
 //    noteDiv.style.width = note.xsize;
@@ -26,9 +27,20 @@ function drawNote(note, zindex) {
     dashboardContainer.appendChild(noteDiv);
 }
 
+
+
+const Colors = {
+    Bluegray: "#d0e0e3",
+    Bluelight: "#a9d5e3",
+    Blue: "#7ebed2",
+    Yellow: "#ffdb91",
+    Orange: "#fec4ae",
+    Pink: "#e9bad3"
+}
+
 window.addEventListener("load", function() {
 
-    const notesObj = JSON.parse(notesRaw);
+    console.log(notes);
 
     const htmlBody = document.getElementsByTagName("body")[0];
     const dashboardContainer = document.getElementById("dashboardContainer");
@@ -39,7 +51,7 @@ window.addEventListener("load", function() {
     let zindex = 100;
     let i = 0;
 
-    for (let note of notesObj.notes) {
+    for (let note of notes) {
         drawNote(note, zindex);
         zindex = zindex + 10;
     }
